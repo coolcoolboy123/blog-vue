8effdc83bc71e9da30b30457e1e3cdb4c25d9081
Summer <87221937+coolcoolboy123@users.noreply.github.com>
2023-05-18 11:06:18 +0800

    Update vue.config.js

--- sg-vue-admin/vue.config.js
+++ sg-vue-admin/vue.config.js
@@ -30,7 +30,7 @@ module.exports = {
     proxy: {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `http://8.134.101.81:8989`,
+        target: `http://*.*.*.*:8989`,
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
